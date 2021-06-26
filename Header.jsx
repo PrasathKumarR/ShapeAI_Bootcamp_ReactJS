25a46b93554b9444e661932e2c9d7662f70c11ad
PrasathKumarR <86519850+PrasathKumarR@users.noreply.github.com>
2021-06-26 17:22:30 +0530

    Add files via upload

---
+++ Header.jsx
@@ -0,0 +1,9 @@
+import React from "react";
+function Header() {
+  return (
+    <header>
+      <h1>Prasath's Project</h1>
+    </header>
+  );
+}
+export default Header;
---
+++ Info.jsx
@@ -0,0 +1,10 @@
+import React from "react";
+function Info() {
+  return (
+    <div class="note">
+      <h1>JavaScript and React.js</h1>
+      <p>First Bootcamp attended By ME</p>
+    </div>
+  );
+}
+export default Info;
